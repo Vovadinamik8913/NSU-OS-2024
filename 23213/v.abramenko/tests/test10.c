df3d6e50ccef1875752e3dba0a2319796fdeaa41
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-11-04 01:33:54 +0700

    test for lab10

---
+++ 23213/v.abramenko/tests/test10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv) {
+
+    if (argc < 2){
+        fprintf(stderr, "not enough arguments\n");
+        exit(-1);
+    }
+
+    int status;
+
+    execvp(argv[1], &argv[1]);
+    perror("execvp failed");
+    exit(-1);
+}
