bf2170b8e9d9615eaef6a2687b9693c7750c46c5
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-11-29 14:39:37 +0700

    new 32

--- 23213/v.abramenko/tests/server.c
+++ 23213/v.abramenko/tests/server.c
@@ -49,13 +49,16 @@ int main() {
 
     int cl;
     struct aiocb requests[MAX_CLIENTS];
-    const struct aiocb* view[MAX_CLIENTS];
+    memset(requests, 0, sizeof(requests));
+    const struct aiocb* info[MAX_CLIENTS];
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        view[i] = &requests[i];
+        info[i] = &requests[i];
     }
+    
     int cnt_requests = 0;
     while (1) {
         int cl = accept(fd, NULL, NULL);
+
         if (cl == -1){
             perror("accept failed");
         } else {
@@ -67,38 +70,37 @@ int main() {
             aio_read(&requests[cnt_requests]);
             cnt_requests++;
         }
-        
-    
-        aio_suspend(view, cnt_requests, NULL);
-        for (int i = 0; i < cnt_requests; i++) {
-            ssize_t rc = aio_return(&requests[i]);
-            if (rc <= 0) {
-                if(rc == -1) {
-                    if (errno == EINPROGRESS) {
-                        aio_read(&requests[i]);
-                        continue;
-                    }
-                    perror("return failed");
+
+        aio_suspend(info, cnt_requests, NULL);
+        for (int i = 0; i < cnt_requests; i++)
+        {
+            int rc = aio_return(info[i]);
+            if (rc == -1)
+            {
+                if (aio_error(info[i]) == EINPROGRESS)
+                {
+                    continue;
                 }
-                char* buf = (char*)requests[i].aio_buf;
-                free(buf);
-                close(requests[i].aio_fildes);
+                perror("return failed");
+
+            } else if (rc == 0) {
+                char* buffer = (char*) info[i]->aio_buf;
+                free(buffer);
+                close(info[i]->aio_fildes);
                 requests[i] = requests[cnt_requests - 1];
                 requests[cnt_requests - 1].aio_fildes = -1;
                 requests[cnt_requests - 1].aio_buf = NULL;
                 cnt_requests--;
                 i--;
             } else {
-                char* buf = (char*)requests[i].aio_buf;
-                buf[rc] = 0;
-                for (int j = 0; j < rc; j++) {
-                    putchar(toupper((unsigned char)buf[j]));
+                char* buf = (char*) requests[i].aio_buf;
+                buf[rc] = '\0';
+                for (int i = 0; i < rc; i++) {
+                    putchar(toupper((unsigned char)buf[i]));
                 }
-                aio_read(&requests[i]);
+                aio_read(info[i]);
             }
         }
-        
-
         
     }
 }
