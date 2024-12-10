ded621c18b1825a364f1af5d6d1767f7d306e0a3
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-12-10 08:58:55 +0700

    30

--- 23213/v.abramenko/tests/client.c
+++ 23213/v.abramenko/tests/client.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/un.h>
-#include <errno.h>
 
 #define BUF_SIZE 1024
+char* socket_path = "socket";
 
-char *socket_path = "./socket";
-
-int main(int argc, char *argv[]) {
+int main(void) {
     struct sockaddr_un addr;
     int fd;
 
@@ -21,35 +20,26 @@ int main(int argc, char *argv[]) {
 
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
 
     if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        perror("connection failed");
-        close(fd);
+        perror("connect failed");
         exit(-1);
     }
 
     char buf[BUF_SIZE];
-    ssize_t bytes_read;
-    while ((bytes_read = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-        ssize_t bytes_written = 0;
-        while (bytes_written < bytes_read) {
-            ssize_t rc = write(fd, buf + bytes_written, bytes_read - bytes_written);
-            if (rc == -1) {
-                perror("write failed");
-                close(fd);
-                exit(-1);
-            }
-            bytes_written += rc;
+    ssize_t rc;
+    while ((rc = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+        if (write(fd, buf, rc) == -1) {
+            perror("write failed");
+            exit(-1);
         }
     }
 
-    if (bytes_read == -1) {
+    if (rc == -1) {
         perror("read failed");
-        close(fd);
         exit(-1);
     }
 
-    close(fd);
     exit(0);
-} 
+}
